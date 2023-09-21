669833cab09c5fe0447f0932e8faf89283f367ab
Cayo Yuzo <cayo.yuzo@gmail.com>
2023-09-21 15:42:26 -0300

    req-7 conlcuído

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,82 @@
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Header() {
   const [showHeader, setShowHeader] = useState(Boolean);
   const [showProfile, setShowProfile] = useState(Boolean);
   const [showSearch, setShowSearch] = useState(Boolean);
+  const [title, setTitle] = useState('');
   const location = useLocation();
-  const whichRender = () => {
-    if (location.pathname === '/') setShowHeader(false);
-    if (location.pathname === '/meals') {
-      setShowHeader(true);
-      setShowProfile(true);
-      setShowSearch(true);
-    }
-    if (location.pathname === '/drinks') {
-      setShowHeader(true);
-      setShowProfile(true);
-      setShowSearch(true);
-    }
-    if (location.pathname === '/meals/d+/') {
-      setShowHeader(true);
-      setShowProfile(true);
-      setShowSearch(true);
-    }
-  };
+  const navigate = useNavigate();
+  if (location.pathname === '/') setShowHeader(false);
+  if (location.pathname === '/meals') {
+    setShowHeader(true);
+    setShowProfile(true);
+    setShowSearch(true);
+    setTitle('Meals');
+  }
+  if (location.pathname === '/drinks') {
+    setShowHeader(true);
+    setShowProfile(true);
+    setShowSearch(true);
+    setTitle('Drinks');
+  }
+  if (location.pathname === `\/meals\/\d+\/`) {
+    setShowHeader(false);
+  }
+  if (location.pathname === `\/drinks\/\d+\/`) {
+    setShowHeader(false);
+  }
+  if (location.pathname === `\/meals\/\d+\//in-progress`) {
+    setShowHeader(false);
+  }
+  if (location.pathname === `\/drinks\/\d+\//in-progress`) {
+    setShowHeader(false);
+  }
+  if (location.pathname === '/profile') {
+    setShowHeader(true);
+    setShowProfile(true);
+    setShowSearch(false);
+    setTitle('Profile');
+  }
+  if (location.pathname === '/done-recipes') {
+    setShowHeader(true);
+    setShowProfile(true);
+    setShowSearch(false);
+    setTitle('Done Recipes');
+  }
+  if (location.pathname === '/favorite-recipes') {
+    setShowHeader(true);
+    setShowProfile(true);
+    setShowSearch(false);
+    setTitle('Favorite Recipes');
+  }
+
+  const handleClickProfile = () => {
+    navigate('/profile');
+  }
+
   return (
-    <header>
-      <img
+    <>
+    {showHeader && <header>
+      {showProfile && <img
         data-testid="profile-top-btn"
         src="../images/profileIcon.svg"
         alt="profile icon"
       />
-      <img
+      }
+      {showSearch && <img
         data-testid="search-top-btn"
         src="../images/searchIcon.svg"
         alt="search icon"
+        onClick={ handleClickProfile }
       />
+      }
       <title data-testid="page-title">
-        Recipes App
+        {title}
       </title>
     </header>
+    }
+  </>
   );
 }
