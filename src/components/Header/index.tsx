6c6f1b0546432f05eb70fb6faff786e756f69798
Cayo Yuzo <cayo.yuzo@gmail.com>
2023-09-21 15:11:03 -0300

    innitial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function Header() {
+  const [showHeader, setShowHeader] = useState(Boolean);
+  const [showProfile, setShowProfile] = useState(Boolean);
+  const [showSearch, setShowSearch] = useState(Boolean);
+  const location = useLocation();
+  const whichRender = () => {
+    if (location.pathname === '/') setShowHeader(false);
+    if (location.pathname === '/meals') {
+      setShowHeader(true);
+      setShowProfile(true);
+      setShowSearch(true);
+    }
+    if (location.pathname === '/drinks') {
+      setShowHeader(true);
+      setShowProfile(true);
+      setShowSearch(true);
+    }
+    if (location.pathname === '/meals/d+/') {
+      setShowHeader(true);
+      setShowProfile(true);
+      setShowSearch(false);
+    }
+  };
+  return (
+    <header>
+      <img
+        data-testid="profile-top-btn"
+        src="../images/profileIcon.svg"
+        alt="profile icon"
+      />
+      <img
+        data-testid="search-top-btn"
+        src="../images/searchIcon.svg"
+        alt="search icon"
+      />
+      <title data-testid="page-title">
+        Recipes App
+      </title>
+    </header>
+  );
+}
